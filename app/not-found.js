f9dc2c2d886471d4c5e4dfca8c33662504c7f937
guilherme_pittner <guilherme_pittner@ethercity.tech>
2025-04-17 16:39:52 -0300

    final commit i guess

---
+++ app/not-found.js
@@ -0,0 +1,12 @@
+export default function NotFound() {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen text-center px-4">
+        <h3 className="text-2xl font-bold mb-4">Esta página ainda não existe!</h3>
+        <p>Mas você está vendo ela, então ela existe...</p>
+        <p>Ela não deveria existir, e para garantir isso ela precisou existir, irônico não?</p>
+        <p>Mas qual é o verdadeiro conceito de existir?</p>
+        <p>Boa, você acessou uma página errada e agora me deixou com uma crise existencial</p>
+      </div>
+    );
+  }
+  
